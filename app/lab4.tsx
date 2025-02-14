e333b50eec55a4f89168a4e083377997ce0a206b
Keona <124313409+keona-dc@users.noreply.github.com>
2025-02-13 17:40:29 -0700

    Update lab 4

--- app/lab4.tsx
+++ app/lab4.tsx
@@ -15,17 +15,18 @@ export default function Lab3() {
     }
 
     return(
-    <View>
-        <Text>Choose the destinations you would like a quote for!</Text>
+    <View style={{padding: 20}}>
+        <Text style={{fontSize: 20, padding: 20, textAlign: 'center'}}>Choose the destinations you would like a quote for!</Text>
         <FlatList
             data={vacationList}
             renderItem={({ item }) => (
                 <View>
                     <Pressable onPress={() => toggleCheck(item.id) }>
-                        <View style={{ borderWidth: 1, borderColor: 'black', padding: 10, margin: 5, backgroundColor: item.selected ? 'green' : 'red' }}>
-                            <Text>{item.id}</Text>
+                        <View style={{ borderWidth: 1, borderColor: 'black', padding: 10, margin: 5, backgroundColor: item.selected ? 'lightgreen' : 'white' }}>
                             <Text>{item.location}</Text>
+                            <Text>{item.price}</Text>
                             <Text>{item.average_yearly_temperature}</Text>
+                            <Text>{item.selected ? '\u2705' : ''}</Text>
                         </View>           
                     </Pressable>
                 </View>
